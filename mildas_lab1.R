10c8dd795a97fa6b8731b8b4c11b346f1ea740b9
Milda Poceviciute <milpo192@li15-4.ida.liu.se>
2018-11-07 14:57:46 +0100

    lab1

---
+++ mildas_lab1.R
@@ -0,0 +1,19 @@
+# Bioinformatics lab 1
+
+## Question 2
+
+
+## Part 1)
+
+## product="RecQ type DNA helicase" 
+
+## Part 2)
+
+# First four amino acids are MVVA, hence the full names of them are
+# Methionine, Valine, Valine, Alanine
+
+
+## Part 4)
+
+# they don't match
+# After reversing and taking complements I get that A is complement of T, G is complement of C  (and vice versa)
